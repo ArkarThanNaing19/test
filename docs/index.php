335d7c723019f34d8eb307536b740f2017a1d071
ArkarThanNaing19 <65581145+ArkarThanNaing19@users.noreply.github.com>
2020-11-16 16:56:19 +0630

    index.php add

---
+++ docs/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Test</title>
+</head>
+
+<body id="home">
+   
+   <h1>Hello World</h1>
+   
+   <?php echo "Arkar" ?>
+  
+
+</body>
+
+</html>
