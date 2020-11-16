07811f13cf0da1585116bf49f3f476a9f35dd287
Arkar <arkarthannaing19@gmail.com>
2020-11-16 14:55:38 +0630

    Project Upload

---
+++ components/portfolio.php
@@ -0,0 +1,93 @@
+<section id="portfolio">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                <h2 class="main-title text-center mb-3">Portfolio</h2>
+
+                <p class="sub-title text-center mb-5">These are some of my projects together coding with my teammates.</p>
+
+                <div align="center">
+                    <button class="btn btn-default filter-button" data-filter="all">All</button>
+                    <button class="btn btn-default filter-button" data-filter="laravel">Laravel</button>
+                    <button class="btn btn-default filter-button" data-filter="wordpress">Wordpress</button>
+                    <button class="btn btn-default filter-button" data-filter="chatbot">Chatbot</button>
+                </div>
+
+            </div>
+
+            <br/>
+
+            <!-- Laravel Project -->
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter laravel">
+                    <img src="./images/satekhunarr.JPG" class="img-fluid" alt="Project Image">
+            </div>
+            
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter laravel">
+                    <img src="./images/willtec.JPG" class="img-fluid" alt="Project Image">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter laravel">
+                    <img src="./images/hub.JPG" class="img-fluid" alt="Project Image">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter laravel">
+                    <img src="http://arkarthannaing.ml/images/mpg.JPG" class="img-fluid" alt="Project Image">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter laravel">
+                <a href="javascript:void(0)" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/no_image.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <!-- Wordpress Project -->
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter wordpress">
+                <a href="javascript:void(0)" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/no_image.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter wordpress">
+                <a href="javascript:void(0)" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/no_image.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter wordpress">
+                <a href="javascript:void(0)" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/no_image.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter wordpress">
+                <a href="javascript:void(0)" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/no_image.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <!-- Chatbot Project -->
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter chatbot">
+                <a href="https://www.facebook.com/satekhunarr.ska" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/ska_chatbot.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter chatbot">
+                <a href="https://www.facebook.com/tajimamm" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/tajima.JPG?" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-6 filter chatbot">
+                <a href="https://www.facebook.com/TWburmesetraditionaldessert" target="_blank">
+                    <img src="http://arkarthannaing.ml/images/tw.jpg" class="img-fluid" alt="Project Image">
+                </a>
+            </div>
+
+        </div>
+    </div>
+</section>
